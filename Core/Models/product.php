785fd3fa666f14108023c2f83e8720e8d6630ec9
ugork1 <karol.ugorcak@students.bfh.ch>
2016-11-11 18:06:06 +0100

    product corrected

--- Core/Models/product.php
+++ Core/Models/product.php
@@ -3,25 +3,20 @@
 require_once ("db.php");
 
 class Product {
-    protected $name = "products";
-    private $product_id, $product_name, $product_model, $product_price;
+
+    protected $dbname = "products";
+    private $name, $price, $descr_en, $descr_de, $descr_fr, $brand_id;
 
     public function __construct()
     {}
 
-    public function insertProduct($product_id, $product_name, $product_model, $product_price)
+    public function update()
     {
-//        $this->db = $db;
-        $this->product_id = $product_id;
-        $this->product_name = $product_name;
-        $this->product_model = $product_model;
-        $this->product_price = $product_price;
-
         // prepare statement with placeholders
-        $statement = DB::getInstance()->doQuery("INSERT INTO products (product_id, product_name, product_model, product_price) VALUES ($product_id, $product_id, $product_id, $product_price)");
+        $statement = DB::getInstance()->doQuery("INSERT INTO products (name, price) VALUES ($this->name, $this->price, $this->descr_en, $this->descr_de, $this->descr_fr, $this->brand_id)");
 
         // bind placeholders to parameters: i/ntegers, d/ouble, s/tring, b/lob
-        $statement->bind_param('issd', $product_id, $product_name, $product_model, $product_price);
+        $statement->bind_param('sisssi', name,  price, descr_en, descr_de, descr_fr, brand_id);
 
         // execute statement
         $statement->execute();
@@ -35,14 +30,13 @@ class Product {
     public function getProduct()
     {
         // send a query and get mysqli_result object
-        $result = DB::getInstance()->query("SELECT * FROM products;");
+        $result = DB::getInstance()->doQuery("SELECT * FROM".$this->dbname);
 
         // get the first result row as associative array
         $row = $result->fetch_assoc();
         // show the row
-        echo $row["product_name"];
-        echo $row["product_model"];
-        echo $row["product_price"];
+        echo $row["name"];
+        echo $row["price"];
 
         // close result
         $result->close();
@@ -50,20 +44,13 @@ class Product {
 
     public function getAllProducts()
     {
-//        return $this->db::doQuery('SELECT * FROM '.$this->name);
-        $result = DB::getInstance()->doQuery("SELECT * FROM products;");
+        $result = DB::getInstance()->doQuery("SELECT * FROM ".$this->dbname);
         while ($products = $result->fetch_object("Product"))
-            echo $products."<br />";
+            var_dump($products);
 
         // close result
         $result->close();
     }
 
-    // build a string with the product attributes
-    public function __toString()
-    {
-        return sprintf("%d) %s %s %d",
-            $this->product_id, $this->product_name, $this->product_model, $this->product_price);
-    }
 }
 ?>
