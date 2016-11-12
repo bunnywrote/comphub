5483c437af797625eae232d40fb63415baab5d7d
ugork1 <karol.ugorcak@students.bfh.ch>
2016-11-12 20:32:34 +0100

    changed index

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <?php
     include('header.php');
+    define("ROOT", realpath($_SERVER["DOCUMENT_ROOT"]));
+    include(ROOT.'\Core\Models\Product.php');
+
+    $products = Product::getAllProducts();
 ?>
+
 <main>
     <section>
-        <?php products();?>
+        <?php
+            foreach($products as $key=>$value):
+                echo '
+                    <article>
+                        <div class="article-image">
+                            <image src="assets/images/placeholder.png">
+                        </div>
+                        <div class="article-description">
+                            <span><h2>'.$value->name.'</h2></span>
+                            <span>Price: '.$value->price.' CHF</span>
+                            <p>Article specification: '.$value->descrEN.'</p>
+                        </div>
+                        <div class="article-buy">
+                            <form action="/finance/purchase.php" method="get">
+                                <p><label>Count</label></p>
+                                <p>
+                                    <input type="number" name="count" min="1" value="1" required>
+                                </p>
+                                <input type="hidden" name="price" value='.$value->price.'>
+                                <input type="hidden" name="id" value='.$value->name.'>
+                                <input class="btn" type="submit" value="Buy">
+                            </form>
+                        </div>
+                    </article>
+                ';
+            endforeach;
+        ?>
     </section>
     <aside>
         <div class="user-login">
