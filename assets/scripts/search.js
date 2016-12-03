6b52d750bfa8d2dadfd71dd078978c857113101d
ugork1 <karol.ugorcak@students.bfh.ch>
2016-12-03 20:15:12 +0100

    js corrected path

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var value = $("#searchWindow").val();
 
         if(value.length > 1) {
-            $.get("/Controllers/SearchController.php?query="+value, function (data) {
+            $.get("/?type=search&query="+value, function (data) {
                 // parse JSON string into JS-Object
                 var products = JSON.parse(data);
 
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
                 // iterate through the items and show them
                 for (var i = 0; i < products.length; i++){
-                    items += '<li>'+ products[i].name +'</li>'
+                    items += '<li><a href=/'+ products[i].url+'>'+ products[i].name +'</li>'
                 }
 
                 // set the html content
