887003663f1d774c1f684f11ba0ea87e3c1ae61c
Denis <mail@shevchenko.ch>
2017-10-20 18:50:55 +0200

    deleted env

--- Environment/dev.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $_ENV["DB_PASS"]= "";
-    $_ENV["DB_HOST"]= "localhost";
-    $_ENV["DB_USER"]= "root";
-    $_ENV["DB_NAME"]= "comphub";
--- Environment/prod.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $_ENV["DB_PASS"]= "D@T@B@nk";
-    $_ENV["DB_HOST"]= "mysql.hostinger.com";
-    $_ENV["DB_USER"]= "u411162283_dns";
-    $_ENV["DB_NAME"]= "u411162283_chub";
